64fd957662709bdf9a3a0a851196157fa4963ef8
Ale Breuer <ale.breuer14@gmail.com>
2022-06-30 13:41:39 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Desafio Provincias Argentinas
+
+
+let provincias
+do{
+    provincias = parseInt(prompt("Ingrese la cantidad de provincias que hay en Argentina"))
+}
+
+while(provincias != 23) //El ciclo se va ejecutar cuando el usuario no ingrese 23.Cuando la condicion sea False(23) el ciclo se deja de ejecutar y se muestra en pantalla 'correcto'.
+alert("Correcto")
