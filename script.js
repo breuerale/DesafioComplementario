6996450b87a41df543506e1f39500c1e54a50cf3
Ale Breuer <ale.breuer14@gmail.com>
2022-06-30 14:06:41 -0300

    Agrego swtitch

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 //Desafio Provincias Argentinas
 
 
-let provincias
+let provincias, pais
 do{
-    provincias = parseInt(prompt("Ingrese la cantidad de provincias que hay en Argentina"))
+    provincias = parseInt(prompt("Ingrese la cantidad de paises que limitan con Argentina"))
+    pais = prompt("Ingrese un pais limitrofe").toLowerCase()
 }
 
-while(provincias != 23) //El ciclo se va ejecutar cuando el usuario no ingrese 23.Cuando la condicion sea False(23) el ciclo se deja de ejecutar y se muestra en pantalla 'correcto'.
+while(provincias != 5) //El ciclo se va ejecutar cuando el usuario no ingrese 5.Cuando la condicion sea False(5) el ciclo se deja de ejecutar y se muestra en pantalla 'correcto'.
 alert("Correcto")
+
+switch(pais) {
+    case "uruguay":
+        alert("Es limitrofe")
+        break;
+    case "paraguay":
+        alert("Es limitrofe")
+        break;
+    case "brasil":
+        alert("Es limitrofe")
+        break;
+    case "chile":
+        alert("Es limitrofe")
+        break;
+    case "bolivia":
+        alert("Es limitrofe")
+        break;
+    default:
+        alert("No es limitrofe")
+}
